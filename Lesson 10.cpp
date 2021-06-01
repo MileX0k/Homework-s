1201fb1f5fe09574bc2aeee1376b3ba15c3bffb2
MileX0k <81148586+MileX0k@users.noreply.github.com>
2021-06-02 02:22:07 +0500

    Add files via upload

---
+++ Lesson 10.cpp
@@ -0,0 +1,52 @@
+﻿#include <cstdio>
+#include <cstring>
+
+bool func(char* str, int n) {
+    for (int i = 0; i < n; i++) {
+        if (str[i] == ')') {
+            for (int j = i - 1; j >= 0; j--) {
+                if (str[j] == '(') {
+                    str[i] = str[j] = '*';
+                    break;
+                    func(str, n);
+                }
+                if (str[j] == '{') {
+                    printf("ERROR");
+                    return false;
+                }
+            }
+        }
+        if (str[i] == '}') {
+            for (int j = i - 1; j >= 0; j--) {
+                if (str[j] == '{') {
+                    str[i] = str[j] = '*';
+                    break;
+                    func(str, n);
+                }
+                if (str[j] == '(') {
+                    printf("ERROR");
+                    return false;
+                }
+            }
+        }
+        if (str[i] == ']') {
+            for (int j = i - 1; j >= 0; j--) {
+                if (str[j] == '[') {
+                    str[i] = str[j] = '*';
+                    break;
+                    func(str, n);
+                }
+                if (str[j] == '(') {
+                    printf("ERROR");
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+int main(void) {
+    char test[] = "(a[i] if(0) [14[],5] [{}])";
+    printf("%s", (func(test, strlen(test))) ? "OK" : "FAIL");
+    return 0;
+}
